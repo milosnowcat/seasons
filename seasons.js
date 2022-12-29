184bde7aa3e9151635341f178b6d618c34dbf7c7
milosnowcat <ramiro.alvarez@rahcode.com>
2022-12-29 12:46:49 -0600

    Update seasons.js

--- seasons.js
+++ seasons.js
@@ -1,55 +1,59 @@
 export function start() {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
+  var today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"), //January is 0!
+    yyyy = today.getFullYear();
 
   today = mm + "/" + dd + "/" + yyyy;
   console.log(today);
 
   season(mm);
+  
+  return(season(mm));
 }
 
 function season(mm) {
   if (mm == 1) {
-    s1();
-  }else{
-    s1();
+    return(s1()); 
+  } else {
+    return(s1());
   }
 }
 
 function s1() {
-  var body = document.getElementById("home");
-  var season =
-    "<!-- https://lenadesign.org/2021/12/22/css-happy-new-year-animation-fireworks/ -->" +
-    '<div class="seasons" id="s1">' +
-    '<div class="seasons s1_happy-new-year">' +
-    '<div class="seasons s1_circle">' +
-    '<div class="seasons s1_clock-bottom"></div>' +
-    "</div>" +
-    '<div class="seasons s1_clock-middle">' +
-    '<div class="seasons s1_clock-face"></div>' +
-    "</div>" +
-    '<div class="seasons s1_clock-top">' +
-    '<div class="seasons s1_roof"></div>' +
-    '<div class="seasons s1_deco"></div>' +
-    '<div class="seasons s1_sides"></div>' +
-    "</div>" +
-    '<div class="seasons s1_clouds"></div>' +
-    '<div class="seasons s1_fireworks">' +
-    '<div class="seasons s1_one"></div>' +
-    "</div>" +
-    '<div class="seasons s1_fireworks-2">' +
-    '<div class="seasons s1_two"></div>' +
-    "</div>" +
-    '<div class="seasons s1_fireworks-3">' +
-    '<div class="seasons s1_two"></div>' +
-    "</div>" +
-    '<div class="seasons s1_fireworks-4">' +
-    '<div class="seasons s1_one"></div>' +
-    "</div>" +
-    '<div class="seasons s1_text">Happy 2023!</div>' +
-    "</div>" +
-    "</div>";
+  var body = document.getElementById("home"),
+    season =
+      "<!-- https://lenadesign.org/2021/12/22/css-happy-new-year-animation-fireworks/ -->" +
+      '<div class="seasons" id="s1">' +
+      '<div class="seasons s1_happy-new-year">' +
+      '<div class="seasons s1_circle">' +
+      '<div class="seasons s1_clock-bottom"></div>' +
+      "</div>" +
+      '<div class="seasons s1_clock-middle">' +
+      '<div class="seasons s1_clock-face"></div>' +
+      "</div>" +
+      '<div class="seasons s1_clock-top">' +
+      '<div class="seasons s1_roof"></div>' +
+      '<div class="seasons s1_deco"></div>' +
+      '<div class="seasons s1_sides"></div>' +
+      "</div>" +
+      '<div class="seasons s1_clouds"></div>' +
+      '<div class="seasons s1_fireworks">' +
+      '<div class="seasons s1_one"></div>' +
+      "</div>" +
+      '<div class="seasons s1_fireworks-2">' +
+      '<div class="seasons s1_two"></div>' +
+      "</div>" +
+      '<div class="seasons s1_fireworks-3">' +
+      '<div class="seasons s1_two"></div>' +
+      "</div>" +
+      '<div class="seasons s1_fireworks-4">' +
+      '<div class="seasons s1_one"></div>' +
+      "</div>" +
+      '<div class="seasons s1_text">Happy 2023!</div>' +
+      "</div>" +
+      "</div>";
   body.insertAdjacentHTML("beforebegin", season);
+  
+  return("🕛");
 }
